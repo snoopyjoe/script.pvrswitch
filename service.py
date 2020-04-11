c56ebba7adfe9189f0ee4eef052f944b9a3a07f0
snoopyjoe <tgarner14@gmail.com>
2020-04-10 22:06:55 -0700

    Update conditional
    Condition for when TV Channels or TV Guide Window is active at boot.

--- service.py
+++ service.py
@@ -1,5 +1,5 @@
-import xbmc
-
+import xbmc,xbmcgui
+xbmc.sleep(1000)
 VisbleWindows = xbmc.getCondVisibility('[Window.IsActive(10700) | Window.IsActive(10702)]')
 VisiblePVR = xbmc.getCondVisibility('!Pvr.IsPlayingTv + System.HasPVRAddon')
 
@@ -16,3 +16,15 @@ while not VisbleWindows:
         xbmc.executeJSONRPC(pvr_toggle_on)
         break
     xbmc.sleep(1000)
+else:
+    if VisiblePVR:
+        xbmc.sleep(1000)
+        pvr_toggle_off = '{"jsonrpc": "2.0", "method": "Addons.SetAddonEnabled", "params": ' \
+                                 '{"addonid": "pvr.iptvsimple", "enabled": false}, "id": 1}'
+        xbmc.executeJSONRPC(pvr_toggle_off)
+        xbmc.sleep(1000)
+        pvr_toggle_on = '{"jsonrpc": "2.0", "method": "Addons.SetAddonEnabled", "params": ' \
+                                '{"addonid": "pvr.iptvsimple", "enabled": true}, "id": 1}'
+        xbmc.executeJSONRPC(pvr_toggle_on)
+    else:
+        xbmcgui.Dialog().dialog.ok('There are no PVR Clients installed', 'Please install to use PVR.')
